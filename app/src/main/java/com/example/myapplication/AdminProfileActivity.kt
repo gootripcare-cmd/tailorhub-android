761ca9cc9dfc57aeb776b4b2ca3e26630f9892ee
dharmik264 <dp9825723837@gmail.com>
2026-03-13 16:05:28 +0530

    Fix missing imports in AdminProfileActivity

--- app/src/main/java/com/example/myapplication/AdminProfileActivity.kt
+++ app/src/main/java/com/example/myapplication/AdminProfileActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.content.Intent
 import android.net.Uri
+import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
